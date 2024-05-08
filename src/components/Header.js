fdf9154e33a1afeae3d5515b072dedd497207b3c
jnopareboateng <jnopareboateng@outlook.com>
2024-05-08 10:25:09 +0000

    Add bottom header and Banner for ad section with a carousel

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <header>
       {/* Top Nav */}
-      <div className="flex items-center bg-blue-600 p-1 flex-grow-0 py-2 ">
+      <div className="flex items-center bg-blue-950 p-1 flex-grow-0 py-2 ">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
             src="https://links.papareact.com/f90"
@@ -31,21 +31,36 @@ const Header = () => {
           </button>
         </div>
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div>
+          <div className="link">
             <p>Hello Josh</p>
-            <p>Account & List</p>
+            <p className="font-extrabold md:text-sm">Account & List</p>
           </div>
-          <div>
+          <div className="link">
             <p>Returns</p>
-            <p>& Orders</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div>
+          <div className="relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+              0
+            </span>
             <ShoppingCartIcon className="h-10" />
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2"></p>
           </div>
         </div>
       </div>
 
       {/* Right */}
+      {/* Bottom Nav */}
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-blue-950">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link">Electronics</p>
+      </div>
     </header>
   );
 };
