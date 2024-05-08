58a3d87f55d1807428a6ca0d55cff739c1aa476b
jnopareboateng <jnopareboateng@outlook.com>
2024-05-08 16:27:04 +0000

    add grid columns for products section

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React from "react";
 // import ProductFeed from './ProductFeed'
-
-function Product({id, title, price,description, category, Image}) {
+import { useState } from "react";
+import Image from "next/image";
+import { StarIcon } from "@heroicons/react/solid";
+// import Currency from "react-currency-formatter";
+function Product({ id, title, price, description, category, image }) {
+  const [rating] = useState(Math.floor(Math.random() * (5 - 1 + 1)) + 1);
+  // random rating between 1 and 5
+  const [hasPrime] = useState(Math.random() < 0.5);
+  function currencyFormat(num) {
+    return "$" + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  }
   return (
-    <div>Product</div>
-  )
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+        {category}
+      </p>
+      <Image src={image} height={200} width={200} objectFit="contain" />
+      <h4>{title}</h4>
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon key={i} className="h-5 text-yellow-500" />
+          ))}
+      </div>
+
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
+      <div>
+        <p>{currencyFormat(price)}</p>
+      </div>
+      {hasPrime && (
+        <div className="flex items-center space-x-2 -mt-5">
+          <img
+            loading="lazy"
+            className="w-12"
+            src="https://links.papareact.com/fdw"
+            alt=""
+          />
+          <p>FREE Next-day Delivery</p>
+        </div>
+      )}
+      <button className="button">Add to Basket</button>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,20 +1,10 @@
 import React from "react";
-import Product from "./Product";
+import Product from "../components/Product";
 function ProductFeed({ products }) {
   return (
-    <div className="text-center font-extrabold">
-      <h1>Products here...</h1>
-      {/* {products.map({
-        id,
-        title,
-        price,
-        description,
-        category,
-        image,
-      })} */}
-      {products.map((product) => {
-        const { id, title,image, price, description,category } = product;
-
+    <div className="grid grid-flow-row-dense md:grid-cols-3 lg:grid-cols-4 text-center font-extrabold">
+      {/* <h1>Products here...</h1> */}
+      {products.map(({ id, title, price, description, category, image }) => (
         <Product
           key={id}
           id={id}
@@ -23,8 +13,10 @@ function ProductFeed({ products }) {
           description={description}
           category={category}
           image={image}
-        />;
-      })}
+        />
+      ))}
+      {/* {products.map((product) => {
+        const { id, title,image, price, description,category } = product; */}
     </div>
   );
 }
