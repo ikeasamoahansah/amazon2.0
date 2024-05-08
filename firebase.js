7e699fce28665f5be1b7551bce44982573a03aee
jnopareboateng <jnopareboateng@outlook.com>
2024-05-08 19:13:57 +0000

    add authentication with firebase and next auth and install their dependencies

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuj8jVmv6XlvUtjuo6H0rdK93gfTvkErw",
+  authDomain: "zilion-ecommerce.firebaseapp.com",
+  projectId: "zilion-ecommerce",
+  storageBucket: "zilion-ecommerce.appspot.com",
+  messagingSenderId: "726377111487",
+  appId: "1:726377111487:web:b53c0f78fddee3d6214b0a",
+  measurementId: "G-P6VXDPCEGH",
+};
