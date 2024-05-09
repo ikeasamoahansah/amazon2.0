3a7500799ea62385bcf4e24fcd15c74f34f84557
jnopareboateng <jnopareboateng@outlook.com>
2024-05-09 08:30:03 +0000

    feat: Add Redux integration to Header and Product components

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,23 +1,43 @@
 import React from "react";
-// import ProductFeed from './ProductFeed'
 import { useState } from "react";
 import Image from "next/image";
 import { StarIcon } from "@heroicons/react/solid";
-// import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "@/slices/basketSlice";
+// import { selectItems } from "@/slices/basketSlice";
+
 function Product({ id, title, price, description, category, image }) {
-  const [rating] = useState(Math.floor(Math.random() * (5 - 1 + 1)) + 1);
-  // random rating between 1 and 5
+  // Dispatch the action to add an item to the basket
+  const dispatch = useDispatch();
+  // Generate a random rating outside of the component
+  const rating = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
   const [hasPrime] = useState(Math.random() < 0.5);
+
   function currencyFormat(num) {
     return "$" + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
   }
+
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      price,
+      description,
+      category,
+      image,
+      rating,
+      hasPrime,
+    };
+    // Sending the product as an action to the REDUX store... the basket slice
+    dispatch(addToBasket(product));
+  };
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
       <p className="absolute top-2 right-2 text-xs italic text-gray-400">
         {category}
       </p>
       <Image src={image} height={200} width={200} objectFit="contain" />
-      <h4>{title}</h4>
+      <h4 className="my-3">{title}</h4>
       <div className="flex">
         {Array(rating)
           .fill()
@@ -27,9 +47,9 @@ function Product({ id, title, price, description, category, image }) {
       </div>
 
       <p className="text-xs my-2 line-clamp-2">{description}</p>
-      <div>
-        <p>{currencyFormat(price)}</p>
-      </div>
+
+      <p className="">{currencyFormat(price)}</p>
+
       {hasPrime && (
         <div className="flex items-center space-x-2 -mt-5">
           <img
@@ -38,10 +58,12 @@ function Product({ id, title, price, description, category, image }) {
             src="https://links.papareact.com/fdw"
             alt=""
           />
-          <p>FREE Next-day Delivery</p>
+          <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
       )}
-      <button className="button">Add to Basket</button>
+      <button onClick={() => addItemToBasket} className="mt-auto button">
+        Add to Basket
+      </button>
     </div>
   );
 }
