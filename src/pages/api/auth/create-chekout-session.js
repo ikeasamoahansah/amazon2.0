2a983bf061019c9eb35a25e12f61da3298ccc6a3
jnopareboateng <jnopareboateng@outlook.com>
2024-05-18 20:00:36 +0000

    Refactor create-checkout-session.js to use Paystack API for payment integration

--- src/pages/api/auth/create-chekout-session.js
+++ src/pages/api/auth/create-chekout-session.js
@@ -1,12 +1,10 @@
 // .../api/create-checkout-session.js
 
 import { getSession } from "next-auth/react";
-import { stripe } from "@/server";
-import { insertOrder } from "@/db";
-import { v4 as uuidv4 } from "uuid";
-import { formatAmountForStripe } from "@/utils/stripe-helpers";
+// import { insertOrder } from "@/db";
+// import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const paystack = require("paystack")(process.env.PAYSTACK_SECRET_KEY);
 
 export default async (req, res) => {
@@ -14,34 +12,43 @@ export default async (req, res) => {
 
   // test if the user is authenticated
   console.log(items, email);
-};
 
-const transformedItems = items.map((item) => ({
-  description: item.description,
-  quantity: 1,
-  price_data: {
-    currency: "&#8373;",
-    unit_amount: formatAmountForStripe(item.price),
-    product_data: {
-      name: item.title,
-      images: [item.image],
+  const transformedItems = items.map((item) => ({
+    name: item.title,
+    amount: item.price * 100, // Paystack uses kobo as the smallest unit
+    quantity: 1,
+    metadata: {
+      custom_fields: [
+        {
+          display_name: "Mobile Number",
+          variable_name: "mobile_number",
+          value: "+233xxxxxxxxx",
+        },
+      ],
     },
-  },
-}));
+  }));
 
-const session = await stripe.checkout.sessions.create({
-  payment_method_types: ["card"],
-  shipping_rates: ["shr_1J9y0vKZJ1m3QXcW"],
-  shipping_address_collection: {
-    allowed_countries: ["GH"],
-  },
-  line_items: transformedItems,
-  mode: "payment",
-  success_url: `${process.env.HOST}/success`,
-  cancel_url: `${process.env.HOST}/checkout`,
-  metadata: {
-    email,
-    images: JSON.stringify(items.map((item) => item.image)),
-  },
-});
+  const response = await axios.post(
+    "https://api.paystack.co/transaction/initialize",
+    {
+      email: email,
+      amount: transformedItems.reduce((total, item) => total + item.amount, 0),
+      currency: "GHS",
+      callback_url: `${process.env.HOST}/success`,
+      metadata: {
+        custom_fields: transformedItems,
+      },
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+      },
+    }
+  );
 
+  if (response.data.status) {
+    res.status(200).json({ id: response.data.data.reference });
+  } else {
+    res.status(400).json({ error: "Failed to create checkout session" });
+  }
+};
