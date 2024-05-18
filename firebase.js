e976004ca3d05ef6c07dbc1119b135cea1cc6554
jnopareboateng <jnopareboateng@outlook.com>
2024-05-18 18:26:49 +0000

    Refactor firebase.js to use environment variables for Firebase configuration

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDuj8jVmv6XlvUtjuo6H0rdK93gfTvkErw",
-  authDomain: "zilion-ecommerce.firebaseapp.com",
-  projectId: "zilion-ecommerce",
-  storageBucket: "zilion-ecommerce.appspot.com",
-  messagingSenderId: "726377111487",
-  appId: "1:726377111487:web:b53c0f78fddee3d6214b0a",
-  measurementId: "G-P6VXDPCEGH",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
