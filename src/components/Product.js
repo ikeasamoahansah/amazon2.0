44de6ed9e2289e18b9a08b271ce218ee194f834d
FoM <fariduoubdamusah@gmail.com>
2024-05-26 22:48:43 +0100

    Add files via upload

--- src/components/Product.js
+++ src/components/Product.js
@@ -33,7 +33,7 @@ function Product({ id, title, price, description, category, image }) {
   };
   return (
     <main className="">
-      <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <div className="relative flex items-center flex-col m-5 bg-white z-30 p-10">
         <p className="absolute top-2 right-2 text-xs italic text-gray-400">
           {category}
         </p>
