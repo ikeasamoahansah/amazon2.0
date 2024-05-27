e00e5742a335c3bd3c2cbfcb980bed7f6ad2ad0b
Joshua Ntow Opare-Boateng <109949311+jnopareboateng@users.noreply.github.com>
2024-05-27 12:48:46 +0100

    comment the orders page out

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { useUser } from "@auth0/nextjs-auth0";
-import Header from "@/components/Header";
+// import React from "react";
+// import { useUser } from "@auth0/nextjs-auth0";
+// import Header from "@/components/Header";
 
-const Orders = ({ orders }) => {
-  const { user, error, isLoading } = useUser();
+// const Orders = ({ orders }) => {
+//   const { user, error, isLoading } = useUser();
 
-  if (isLoading) return <div>Loading</div>;
-  if (error) return <div>{error.message}</div>;
-  return (
-    <div>
-      <Header/>
-      <main className="max-w-screen-lg max-auto p-10">
-        <h1 className="text-3xl">Your Orders</h1>
-        {user ? <h2>orders</h2> : <h2>Please sign in to see your orders</h2>}
-        <div className="mt-5 space-y-4"></div>
-      </main>
-    </div>
-  );
-};
+//   if (isLoading) return <div>Loading</div>;
+//   if (error) return <div>{error.message}</div>;
+//   return (
+//     <div>
+//       <Header/>
+//       <main className="max-w-screen-lg max-auto p-10">
+//         <h1 className="text-3xl">Your Orders</h1>
+//         {user ? <h2>orders</h2> : <h2>Please sign in to see your orders</h2>}
+//         <div className="mt-5 space-y-4"></div>
+//       </main>
+//     </div>
+//   );
+// };
 
-export default Orders;
+// export default Orders;
