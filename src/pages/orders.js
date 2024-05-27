f753e277faec044d33e4a1811066f3b9dfce98f4
Joshua Ntow Opare-Boateng <109949311+jnopareboateng@users.noreply.github.com>
2024-05-27 12:52:35 +0100

    recomment the order page

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -1,8 +1,9 @@
 // import React from "react";
 // import { useUser } from "@auth0/nextjs-auth0";
+// import { getSession } from "@auth0/nextjs-auth0";
 // import Header from "@/components/Header";
 
-// const Orders = ({ orders }) => {
+// const orders = ({ orders }) => {
 //   const { user, error, isLoading } = useUser();
 
 //   if (isLoading) return <div>Loading</div>;
@@ -12,11 +13,24 @@
 //       <Header/>
 //       <main className="max-w-screen-lg max-auto p-10">
 //         <h1 className="text-3xl">Your Orders</h1>
-//         {user ? <h2>orders</h2> : <h2>Please sign in to see your orders</h2>}
-//         <div className="mt-5 space-y-4"></div>
+//         {user ? <h2>orders</h2> : <h2>Please signin to see your orders</h2>}
+//       <div className="mt-5 space-y-4"></div>
 //       </main>
 //     </div>
 //   );
 // };
 
-// export default Orders;
+// export default orders;
+
+// export async function getServerSideProps(context){
+//     const paystack = require('paystack')(process.env.PAYSTACK_SECRET_KEY)
+// }
+
+
+const orders = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+export default orders;
